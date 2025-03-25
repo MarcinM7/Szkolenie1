000cf47b6a1bc4a87c93b4c1ece5d4b182577b52
MarcinM7 <ithetorn123@gmail.com>
2025-03-25 15:29:53 +0100

    karuzela

---
+++ Karuzela/script.js
@@ -0,0 +1,21 @@
+const right_arrow = document.querySelector('.right');
+let active_item = 0;
+const carousel = document.querySelector('.carusel');
+const carousel_items = carousel.children;
+
+function setItem(){
+    console.log(carousel_items);
+
+    for(const carousel_item of carousel_items)
+        console.log('carousel_item', carousel_item);
+    carousel_item.style.opacity = 0;
+
+} 
+
+
+carousel_items[active_item].style.opacity = 1;
+
+right_arrow.addEventListener('click', function() {
+})
+
+setItem();
