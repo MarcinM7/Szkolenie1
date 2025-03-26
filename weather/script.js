314b0a2b0990bb5c11f871bdbb19f3e7255d6ed2
MarcinM7 <ithetorn123@gmail.com>
2025-03-26 15:39:04 +0100

    weather

---
+++ weather/script.js
@@ -0,0 +1,11 @@
+const base_url = 'https://api.weaterapi.com/v1'
+
+const key = '56fa909c22c34441901180038252503'
+
+const url = base_url + '/forecast.json?key=' + key + '&q=Torun&days=5'
+
+console.log(url)
+
+fetch(url)
+.then(data => data.json())
+.then(data => console.log(data))
